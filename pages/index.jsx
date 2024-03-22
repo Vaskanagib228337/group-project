c3f72dbfe8383d80646daf3b72107b49f66d365e
qss_ssds <78267033+Vaskanagib228337@users.noreply.github.com>
2024-03-22 18:38:46 +0500

    group project

---
+++ pages/index.jsx
@@ -0,0 +1,18 @@
+
+const Footer = () => {
+    return (
+        <div>
+            <h1 className="KT">КОНТАКТНАЯ ИНФОРМАЦИЯ</h1>
+            <h1 className="telefon">ТЕЛЕФОН:</h1>
+            <h1 className="nomer">+7 (888) 999-99-99</h1>
+            <h1 className="email">EMAIL:</h1> 
+            <h1 className="vahEmail">ВАШ EMAIL</h1>
+            <button className="buttonZ">Заказать звонок</button>
+            <h1 className="basementSite">Название компании, 1488 | Разработчик сайта</h1>
+
+        </div>
+        
+    )
+}
+
+export default Footer
